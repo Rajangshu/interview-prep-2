1b54bc1a8c94fb853b2b996a83739923e80b8c85
Rajangshu <rajangshukumar@gmail.com>
2025-06-30 12:57:59 +0530

    fixed alias in tsconfig

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint:{
-    ignoreDuringBuilds:true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  typescript:{
-    ignoreBuildErrors:true,
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
